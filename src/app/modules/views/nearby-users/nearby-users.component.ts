c6fd78281296e0f8b808adee578763d3ef4f7e5b
Julian Maldonado <julianmaldonadoag@gmail.com>
2023-04-23 13:32:23 -0500

    Send current location every minute

--- src/app/modules/views/nearby-users/nearby-users.component.ts
+++ src/app/modules/views/nearby-users/nearby-users.component.ts
@@ -11,6 +11,7 @@ export class NearbyUsersComponent implements OnInit {
   lat: number = 0
   lon: number = 0
   nearbyUsers: Array<any> = []
+  intervalId: any
   constructor(
     private websocket: SocketWebServiceService,
     private router: Router
@@ -40,27 +41,38 @@ export class NearbyUsersComponent implements OnInit {
 
   startGeoloc() {
     if (navigator.geolocation) {
-      navigator.geolocation.watchPosition((position) => {
-        var latitud = position.coords.latitude;
-        var longitud = position.coords.longitude;
-        this.lat = latitud
-        this.lon = longitud
-        console.log(`new location read: lat=${latitud};lon=${longitud}`)
-        // Enviar la geolocalización al servidor
-        // socket.emit('ubicacion', {lat: latitud, lon: longitud});
-        let userId = localStorage.getItem('user_id')
-        if (userId == null) {
-          console.log('ERROR IN SESSION DATA: No user_id was found in local storage. No geolocation data will be sent to server.')
-          return
-        }
 
-        this.websocket.emitLocation({ user_id: parseInt(userId), lat: latitud, lon: longitud })
-      });
+      const sendLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+          var latitud = position.coords.latitude;
+          var longitud = position.coords.longitude;
+          this.lat = latitud
+          this.lon = longitud
+          console.log(`new location read: lat=${latitud};lon=${longitud}`)
+          // Enviar la geolocalización al servidor
+          // socket.emit('ubicacion', {lat: latitud, lon: longitud});
+          let userId = localStorage.getItem('user_id')
+          if (userId == null) {
+            console.log('ERROR IN SESSION DATA: No user_id was found in local storage. No geolocation data will be sent to server.')
+            return
+          }
+  
+          this.websocket.emitLocation({ user_id: parseInt(userId), lat: latitud, lon: longitud })
+        });
+      }
+
+      // Ejecutar la función de envío de ubicación cada segundo
+      this.intervalId = setInterval(sendLocation, 1000);
 
       console.log('whatching geolocalization')
     } else {
       console.log('Geolocalización no es soportada por este navegador');
     }
+  }
+
+  stopGeoloc() {
+    // Detener el envío de ubicación al servidor
+    clearInterval(this.intervalId);
   }
 
   goToPage(path: string) {
